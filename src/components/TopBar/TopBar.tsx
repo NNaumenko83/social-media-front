724e4430f5da32de769aa10b92f60914325e3347
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2024-07-13 18:08:12 +0300

    UPD: TopBar

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,7 +1,47 @@
 import css from './TopBar.module.css'
+import { Search, Person, Chat, Notifications } from '@mui/icons-material'
+import PersonImage from '../../assets/person/1.jpeg'
 
 const TopBar = () => {
-    return <div>TopBar</div>
+    return (
+        <div className={css.topbarContainer}>
+            <div className={css.topbarLeft}>
+                <span className={css.logo}>Social</span>
+            </div>
+
+            <div className={css.topbarCenter}>
+                <div className={css.searchBar}>
+                    <Search />
+                    <input
+                        type="text"
+                        className={css.searchInput}
+                        placeholder="Search for friend, post or video"
+                    />
+                </div>
+            </div>
+            <div className={css.topbarRight}>
+                <div className={css.topbarLinks}>
+                    <span className={css.topbarLink}>Homepage</span>
+                    <span className={css.topbarLink}>Timeline</span>
+                </div>
+                <div className={css.topbarIcons}>
+                    <div className={css.topbarIconItem}>
+                        <Person />
+                        <span className={css.topbarIconBadge}>1</span>
+                    </div>
+                    <div className={css.topbarIconItem}>
+                        <Chat />
+                        <span className={css.topbarIconBadge}>2</span>
+                    </div>
+                    <div className={css.topbarIconItem}>
+                        <Notifications />
+                        <span className={css.topbarIconBadge}>1</span>
+                    </div>
+                </div>
+                <img src={PersonImage} alt="" className={css.topbarImage} />
+            </div>
+        </div>
+    )
 }
 
 export default TopBar
