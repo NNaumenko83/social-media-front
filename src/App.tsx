89bbf76e7212fb4bb22c229f0d3c67b8331518b2
Mykola <nnv2006@ukr.net>
2024-07-19 14:38:35 +0300

    ADD: routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-// import Login from './pages/Login/Login'
-// import Register from './pages/Register/Register'
-// import Profile from './pages/Profile/Profile'
+import { Routes, Route } from 'react-router-dom'
 
+import Login from './pages/Login/Login'
+import Register from './pages/Register/Register'
+import Profile from './pages/Profile/Profile'
 import Home from './pages/Home/Home'
-
-// import Home from './pages/Home/Home'
+import NotFound from './pages/NotFound/NotFound'
 
 function App() {
     return (
         <>
-            <Home />
-            {/* <Profile /> */}
-            {/* <Login /> */}
-            {/* <Register /> */}
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/profile/:username" element={<Profile />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
         </>
     )
 }
